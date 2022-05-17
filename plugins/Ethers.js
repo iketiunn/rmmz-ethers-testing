69ee825572fe81cd9af1c783b27ea744bac7d39c
iketiunn <ike.chang@sideeffect.dev>
2022-05-17 08:35:17 +0800

    feat: register commands sign_in, get_user_assets

--- plugins/Ethers.js
+++ plugins/Ethers.js
@@ -8,49 +8,133 @@
  * @author iketiunn
  *
  * @help Ethers.js
- *
+ * 
  * This plugin pops wallet connect to make you login with your wallet
  * It adds the ability to use ethers.js in your game.
  * 
  * ethers.js docs: https://docs.ethers.io/v5/
+ * 
+ * @param DEV_PRIVATE_KEY
+ * @text Wallet private key
+ * @type string
+ * @desc Wallet private key during developing.
+ * @default ""
+ * 
+ * @command sign_in
+ * @text Sign in wallet
+ * @desc Sign in wallet with different methods
+ *
+ * @command get_user_assets
+ * @text Get user's assets
+ * @desc Get user's assets from the specific contract
+ */
+(async () => {
+  const FILE_NAME = 'Ethers'
+  const userParams = PluginManager.parameters(FILE_NAME)
+  const DEV_PRIVATE_KEY = userParams['DEV_PRIVATE_KEY'] || ""
+
+  /**
+   * Private Variables:
+   * 
+   * starts with $ -> user defined
+   * start with _ -> system defined, not not change it
+   */
+  const provider = ethers.getDefaultProvider("homestead", {
+    etherscan: "" // TODO: remove it before upload
+  });
+  let $privateKey = DEV_PRIVATE_KEY // Or user input
+  let _wallet
+  // pseudo abi and address from BYAC
+  const $contractAddress = '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d'
+  const _abi = await fetch(`https://api.etherscan.io/api?module=contract&action=getabi&address=${$contractAddress}`)
+    .then(res => res.json())
+    .then(json => json.result)
+  console.debug($contractAddress, _abi)
+  const _contract = new ethers.Contract($contractAddress, _abi, provider);
+
+  /**
+   * Main
+   */
+  PluginManager.registerCommand(FILE_NAME, "sign_in", async args =>
+    // Chose a way to sign in, private key only now
+    _wallet = await signIn({
+      privateKey: $privateKey
+    })
+  )
+
+  PluginManager.registerCommand(FILE_NAME, "get_user_assets", async args => {
+    const somebodyAddress = '0xF4Fb9FA23edB32215E5284cf7dBfDB5607d51a5b' // TODO: Should current signer
+    const assets = await getUsersAssets(_contract, somebodyAddress)
+    // const assets = await getUsersAssets(_contract, _wallet.address)
+    console.log(assets)
+    const img = ImageManager.loadBitmapFromUrl(assets[0].imageUrl)
+    console.log(img)
+    // Game_Picture.prototype.showPicture(
+    //   911,
+    //   "https://cloudflare-ipfs.com/ipfs/QmbvtcMPRCCR595DpRtHfvFvTcKNAo7B4eWc5wicSp2rjE",
+    //   0,
+    //   0,
+    //   0, 100, 100, 255, 0
+    // )
+  })
+})()
+
+/**
+ * Private functions
  */
 
-// Try to init a wallet connect information
-// Create random wallet first for testing
-let wallet = ethers.Wallet.createRandom();
-console.log('address:', wallet.address)
-console.log('mnemonic:', wallet.mnemonic.phrase)
-console.log('privateKey:', wallet.privateKey)
+async function signIn({
+  privateKey
+}) {
+  if (privateKey !== "") {
+    wallet = new ethers.Wallet(privateKey);
+    console.log('Load wallet from given private key')
 
-// Default to Ethereum https://docs.ethers.io/v5/api/providers/
-// For real use case, provider should be a authenticated wallet
-// const provider = new ethers.providers.EtherscanProvider('homestead', ''); // For testing with api provider, get the api key from https://etherscan.io/myapikey
-
-// pseudo abi and address from BYAC
-const abi = [{"inputs":[{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"symbol","type":"string"},{"internalType":"uint256","name":"maxNftSupply","type":"uint256"},{"internalType":"uint256","name":"saleStart","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"BAYC_PROVENANCE","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"MAX_APES","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"REVEAL_TIMESTAMP","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"apePrice","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"baseURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"emergencySetStartingIndexBlock","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"flipSaleState","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxApePurchase","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"numberOfTokens","type":"uint256"}],"name":"mintApe","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"reserveApes","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"saleIsActive","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"baseURI","type":"string"}],"name":"setBaseURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"provenanceHash","type":"string"}],"name":"setProvenanceHash","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"revealTimeStamp","type":"uint256"}],"name":"setRevealTimestamp","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"setStartingIndex","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"startingIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"startingIndexBlock","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}]
-const contractAddress = '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d'
-const contract = new ethers.Contract(address, abi, wallet);
-// const contract = new ethers.Contract(contractAddress, abi, provider); // TODO: contract with default provider for testing
-/** Get all NFT address from owner, and get all asset metadata one by one */
-const somebodyAddress = '0x5f7Bd8E190D30b9Db5656749c745b8988aB69Cd0'
-contract.balanceOf(somebodyAddress).then(async balance => {
-  console.log('total balance:', balance)
-  for (let i = 0; i < balance; i++) {
-    const tokenId = await contract.tokenOfOwnerByIndex(somebodyAddress, i)
-    const uri = await contract.tokenURI(tokenId)
-    console.log('uri', uri)
-    const url = `https://ipfs.io/ipfs/${uri.split("ipfs://")[1]}` // TODO: temp, find a proper gateway
-    const json = await fetch(url).then(res => res.json()) // TODO: handle error
-    console.log(json)
-    const imageUri = json.image
-    const imageUrl = `https://cloudflare-ipfs.com/ipfs/${imageUri.split("ipfs://")[1]}` // TODO: temp, find a proper gateway
-    console.log('image url:', imageUrl)
-    // Also you can use the tokenId to map to the in-game item
+    return wallet
   }
-})
+
+  wallet = ethers.Wallet.createRandom();
+  console.log('Random wallet created...')
+  console.debug('address:', wallet.address)
+  console.debug('mnemonic:', wallet.mnemonic.phrase)
+  console.debug('privateKey:', wallet.privateKey)
+
+  return wallet
+}
+
+async function getCollectionsFromMarket() {
 
 
-// Map to related in-game item
+}
 
-// ...
-// ...
+async function getUsersAssets(contract, userAddress) {
+  const assets = []
+  await contract.balanceOf(userAddress).then(async balance => {
+    console.debug(userAddress, 'total balance:', balance)
+    for (let i = 0; i < balance; i++) {
+      const tokenId = await contract.tokenOfOwnerByIndex(userAddress, i)
+      const uri = await contract.tokenURI(tokenId)
+      const url = `https://ipfs.io/ipfs/${uri.split("ipfs://")[1]}` // TODO: temp, find a proper gateway
+      const json = await fetch(url).then(res => res.json()) // TODO: handle error
+      const imageUri = json.image
+      const imageUrl = `https://cloudflare-ipfs.com/ipfs/${imageUri.split("ipfs://")[1]}` // TODO: temp, find a proper gateway
+      // Also you can use the tokenId to map to the in-game item
+      assets.push({
+        ...json,
+        imageUrl
+      })
+    }
+  })
+
+  return assets
+}
+
+async function showUserAssets() {
+  
+}
+
+
+async function mintRandomNFT(contract, userAddress) {
+
+
+}
